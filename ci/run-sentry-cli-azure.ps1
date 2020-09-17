41204b76ecad6d0f94c3ad89231aba019b2bd5cd
Vladimir Sumarov <summeroff@gmail.com>
2020-09-17 15:45:22 -0700

    fix

--- ci/run-sentry-cli-azure.ps1
+++ ci/run-sentry-cli-azure.ps1
@@ -8,17 +8,12 @@ $output = "$env:ROOTDIRECTORY\dump_syms.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
-dir $env:ROOTDIRECTORY
-
-dir $env:PDBPATH
-
 .\dump_syms.exe $env:PDBPATH\color_picker.pdb  > $env:PDBPATH\color_picker.sym
 
-.\dump_syms.exe
-dir $env:PDBPATH
 .\sentry-cli.exe upload-dif --log-level DEBUG --org streamlabs-obs --project obs-client $env:PDBPATH\color_picker.sym
-dir $env:PDBPATH
+
 .\sentry-cli.exe upload-dif --log-level DEBUG --org streamlabs-obs --project obs-client -t pdb $env:PDBPATH\color_picker.pdb
 
-dir $env:PDBPATH
+dir $env:PDBPATH\
 
+del $env:PDBPATH\color_picker.pdb
