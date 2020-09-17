961839504bb680cdaf3616e46ed9a645f9303436
Vladimir Sumarov <summeroff@gmail.com>
2020-09-17 14:47:42 -0700

    use dumpsyms

--- ci/run-sentry-cli-azure.ps1
+++ ci/run-sentry-cli-azure.ps1
@@ -3,6 +3,12 @@ $output = "$env:ROOTDIRECTORY\sentry-cli.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
+$url = "https://github.com/google/breakpad/blob/master/src/tools/windows/binaries/dump_syms.exe"
+$output = "$env:ROOTDIRECTORY\dump_syms.exe"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+
+
 Get-ChildItem -Path $env:PDBPATH\color-picker -Filter *.pdb -File | ForEach-Object {.\dump_syms.exe $_.FullName > $env:ROOTDIRECTORY\syms\$_} 
 Get-ChildItem -Path $env:PDBPATH\color-picker -Filter *.pdb -Recurse -File | ForEach-Object {.\dump_syms.exe $_.FullName > $env:ROOTDIRECTORY\syms\$_}
 Get-ChildItem -Path $env:ROOTDIRECTORY\syms\*.pdb | Rename-Item -NewName { $_.Name -Replace ".pdb",".sym"}
